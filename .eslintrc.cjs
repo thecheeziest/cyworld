dc16515ba98f61fd8cc404d068f0df37114e9bb8
thecheeziest <followzoa@gmail.com>
2024-06-24 13:39:23 +0900

    chore: 빌드 및 배포 오류 재설정, 종속성 업데이트

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,4 @@
 /* eslint-env node */
-
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
@@ -12,6 +11,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    "no-unused-vars": "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
