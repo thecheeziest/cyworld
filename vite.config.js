9ec845e01c86dbbded4f0e17f562d38121b16967
thecheeziest <followzoa@gmail.com>
2024-06-24 13:55:05 +0900

    docs: 서버 설정 mimeTypes 추가

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // 서버 설정 추가
+    mimeTypes: {
+      "text/javascript": ["js", "jsx"],
+      "text/jsx": ["jsx"]
+    }
+  }
 })
